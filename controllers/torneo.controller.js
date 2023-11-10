26733b47dbad5aadf12ce02323cc76814b761255
KillerFerd <91443717+KillerFerd@users.noreply.github.com>
2023-11-10 02:48:15 -0600

    Finalizar agregación de torneos

--- controllers/torneo.controller.js
+++ controllers/torneo.controller.js
@@ -1,4 +1,5 @@
-const { Torneo, EstadoTorneo } = require("../db/models");
+const { Torneo, EstadoTorneo, sequelize } = require("../db/models");
+const { Op } = require('sequelize');
 
 module.exports.getTorneos = async (req, res) => {
   try {
@@ -29,21 +30,52 @@ module.exports.getTorneos = async (req, res) => {
 
 module.exports.createTorneo = async (req, res) => {
   try {
+    const { nombre, fechaInicio, fechaFin } = req.body;
+
+    // Verificar si hay algún torneo activo para la nueva fecha de inicio
+    const isFechaInicioValid = await Torneo.findOne({
+      where: {
+        [Op.or]: [
+          {
+            fechaInicio: {
+              [Op.between]: [fechaInicio, fechaFin],
+            },
+          },
+          {
+            fechaFinFinal: {
+              [Op.between]: [fechaInicio, fechaFin],
+            },
+          },
+        ],
+      },
+    });
+    
+    if (isFechaInicioValid) {
+      return res.status(400).json({
+        message: "Ya existe un torneo para la fecha de inicio actual",
+        data: isFechaInicioValid,
+      });
+    }
+
+    // Crear el nuevo torneo
     const [torneo, created] = await Torneo.findOrCreate({
       where: {
-        nombre: req.body.nombre,
+        nombre,
       },
       defaults: { ...req.body },
     });
+
     if (!created) {
       return res.status(409).json({
         message: "El Torneo ya existe en la Base de Datos",
         data: torneo,
       });
     }
-    return res.status(201).json({ 
-      message: "El Torneo se agrego exitoxamente",
-      data: torneo });
+
+    return res.status(201).json({
+      message: "El Torneo se agregó exitosamente",
+      data: torneo,
+    });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- db/seeders/20231012041040-seed-torneo.js
+++ db/seeders/20231012041040-seed-torneo.js
@@ -5,16 +5,16 @@ module.exports = {
     return queryInterface.bulkInsert('torneo', [
       {
         nombre: 'Torneo de prueba',
-        fecha_inicio: '2022-10-10',
-        fecha_fin: '2022-11-10',
+        fecha_inicio: '2023-11-12',
+        fecha_fin: '2023-12-02',
         id_estado: 1, 
         created_at: new Date(),
         updated_at: new Date()
       },
       {
         nombre: 'Torneo de prueba2',
-        fecha_inicio: '2022-10-10',
-        fecha_fin: '2022-11-10',
+        fecha_inicio: '2023-11-12',
+        fecha_fin: '2023-12-02',
         id_estado: 1, 
         created_at: new Date(),
         updated_at: new Date()
